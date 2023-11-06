979cbbef9e1bf161ac402252e250bff856bc90c8
racheljeong <jdsss2634@gmail.com>
2023-11-06 20:38:51 +0900

    testing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SWRConfig } from 'swr';
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <SWRConfig value={{fetcher : (url : string) => fetch(url).then((response) => response.json())}}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <main
