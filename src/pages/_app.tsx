f7f09ac037bdb9f1f7f2607ee6ac0fb07478e182
racheljeong <jdsss2634@gmail.com>
2023-11-12 23:04:09 +0900

    modified min-height

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     //max-w-xl 
   <SWRConfig value={{fetcher : (url : string) => fetch(url).then((response) => response.json())}}>
-    <div className='w-full h-full max-w-3xl m-auto bg-slate-50'>
+    <div className='w-full h-full min-h-screen max-w-3xl m-auto bg-slate-50'>
       <Component {...pageProps}/>
     </div>
   </SWRConfig>
